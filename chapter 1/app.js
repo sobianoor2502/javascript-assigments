f1babf8cc48df53a96ef3b99a254a4653e548f5e
sobianoor2502 <sobianoor2502@gmail.com>
2026-02-28 00:09:24 -0800

    chapter 1 & 2 complete

---
+++ chapter 1/app.js
@@ -0,0 +1,16 @@
+// Question 1
+alert("hello");
+
+// Question 2
+alert("Error! Please enter a valid pasword.");
+
+// Question 3
+alert("Welcome to js land... \n Happy Coding!");
+
+// Question 4
+alert("Welcome to js land...");
+alert("Happy Coding!");
+
+// Question 5
+alert("Hello... Can I run JS througn my web browser's console");
+console.log("Hello... Can I run JS througn my web browser's console");
---
+++ chapter 2/app.js
@@ -0,0 +1,43 @@
+// question 1
+var username;
+
+// question 2
+var myName = "Hafsa Imran";
+
+// question 3
+// a)
+var message;
+// b)
+var message = "Hello World";
+// c)
+alert(message);
+
+// question 4
+var studentName = "Jhone Doe";
+var studentAge = 15 + " years old";
+var studentCourse = "Certified Mobile Application Development";
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+// question 5
+var item = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(item);
+
+// question 6
+var email = "My email is " + "example@example.com";
+alert(email);
+
+// question 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn "+book);
+
+// question 8
+var text = "JavaScript"
+document.write("Yah! I can write HTML content through" + text);
+document.write("<br \>");
+
+// question 9
+let pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(pattern);
+document.write(pattern);
