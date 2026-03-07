b3b1b7b72f61aacdae4afdab1bf7b199c69e49b4
sobianoor2502 <sobianoor2502@gmail.com>
2026-03-07 00:55:15 -0800

    chapter 7

---
+++ chapter 6/app.js
@@ -0,0 +1,46 @@
+// Question 01
+
+// let a=10;
+// document.write(`Result <br/>`)
+// document.write(`The value of a is $a} <br/>`);
+// document.write(`---------------------------------- <br/>`);
+
+// document.write(`The value of +a is: ${+a} <br/>`);
+// document.write(`Now the value of a is: $a}`);
+
+// document.write(`<br/>`);
+// document.write(`<br/>`);
+// document.write(`The value of a++ is: $a++} <br/>`);
+// document.write(`Now the value of a is: $a}`);
+
+// document.write(`<br/>`);
+// document.write(`<br/>`);
+// document.write(`The value of -a is: ${-a} <br/>`);
+// document.write(`Now the value of a is: $a}`);
+
+// document.write(`<br/>`);
+// document.write(`<br/>`);
+// document.write(`The value of a-- is: $a--} <br/>`);
+// document.write(`Now the value of a is: $a}`);
+
+// Question 02
+
+// let a=2;
+// let b=1;
+
+// document.write(`a is: ${a} <br/>`);
+// document.write(`b is: ${b} <br/>`);
+
+// document.write(`Result is: ${a++} <br/>`);
+// 2
+
+// document.write(`Result is: ${--a - --b + ++b} <br/>`);
+// 1 -  0  +  1 = 0
+
+// document.write(`Result is: ${--a - --b + ++b + b--} <br/>`);
+//  1 -  0  +  2  +  1
+
+// Question 03
+
+let userName = prompt("Enter Your Name");
+document.write(`Hey: ${userName}`);
