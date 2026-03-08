e443dd84d205cebaee5ac77cb6670c2823ec6947
sobianoor2502 <sobianoor2502@gmail.com>
2026-03-08 01:01:16 -0800

    half done

---
+++ chapter 8/app.js
@@ -0,0 +1,17 @@
+// let  = +prompt("enter your name")
+// if(num%2===0){
+//     alert(`${num} is an even number`)
+// }
+
+// else{
+//     alert(`${num} is an odd number`)
+// }
+
+let num = +prompt("enter your name")
+if(num%5===0){
+    alert(`${num} is an even number`)
+}
+
+else{
+    alert(`${num} is an odd number`)
+}
